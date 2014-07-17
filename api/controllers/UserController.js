75f32b472c5d2894f78a4cb858494d813f5a1327
DarthHater <jeffryxtron@gmail.com>
2014-07-16 21:08:33 -0400

    Adding number of threads/posts per user

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -4,22 +4,36 @@
  * @description :: Server-side logic for managing Users
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
-var db = require('../services/db');
+var db = require('../services/db'),
+    Q = require('q');
 
 module.exports = {
   /**
    * `UserController.view()`
    */
+
   view: function (req, res) {
     var id = req.param('id');
 
     db.User.findOne({ _id: id }).lean().exec(function (err, user) {
       if (err) return res.json('SHIT DONE FUCKED UP', 500);
 
-      return res.json(
-        { user: user }, 
-          200
-          );
+      var countThreads = db.Thread.count({ creator: id }).exec();
+      var countPosts = db.Post.count({ creator: id}).exec();
+
+      var data = Q.all([
+        countThreads,
+        countPosts]).then(function(data) {
+
+          user.threads = data[0];
+          user.posts = data[1];
+          return res.json(
+            { user: user }, 
+              200
+              );
+        });
+
+      
     });
   },
 };
