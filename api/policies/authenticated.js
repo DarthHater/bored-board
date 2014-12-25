a05dcb93cc53230480a0bb4e94d4497b9fb1f0e2
Jeffry Hesse <jeffryxtron@gmail.com>
2014-12-24 16:41:36 -0900

    Changing from 403 to 401 since 401 is technically the better response if you aren't authenticated

--- api/policies/authenticated.js
+++ api/policies/authenticated.js
@@ -12,6 +12,6 @@ module.exports = function(req, res, next) {
   if (req.isAuthenticated()){
     return next();
   } else {
-    return res.send(403, { message: 'Not Authorized' });
+    return res.send(401, { message: 'Not Authorized' });
   }
 };
